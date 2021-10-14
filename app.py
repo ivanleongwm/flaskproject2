6b0a39c9941d3e0d1503642d333d68a3d2eda19f
ivanleongwm <ivanleongwm@gmail.com>
2021-10-14 22:20:32 +0800

    20211014 Code that passes python dictionary to html elements via jinja2 to javascript

---
+++ app.py
@@ -0,0 +1,38 @@
+import datetime
+from flask import Flask, render_template, request
+import json
+
+
+app = Flask(__name__)
+#client = MongoClient(r"mongodb+srv://ivanleongwm:Ivan1904@microblog-application.utvq0.mongodb.net/test")
+#app.db = client.microblog
+entries = []
+
+@app.route("/", methods=["GET","POST"])
+def home():
+    #print([e for e in app.db.entries.find({})])
+    if request.method == "POST":
+        entry_content = request.form.get("content")
+        formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+        entries.append((entry_content,formatted_date))
+
+    entries_with_date = [
+        (
+            entry[0],
+            entry[1],
+            datetime.datetime.strptime(entry[1],"%Y-%m-%d").strftime("%b %d")
+        )
+        for entry in entries
+    ]
+
+    color = ['Red','Black','Blue','Orange']
+
+    dictionary = {
+        'Vincent':[["SPPSG Recon","Nexus Script to automate posting of JE"],["Lumos Query Error","Fixing of syntax errors in lumos"]],
+        'Tiffany':[["Invoice generator","Using python and vba to generate invoices"],["File Lag","Aggregated rows"]]
+    }
+
+    javascript_data = [1,"foo"]
+
+    return render_template("home.html", entries=entries_with_date, colours=color, dictionary=dictionary, data=json.dumps(javascript_data), dictionary2=dictionary)
+
